ff86c1add34a1d1250a45762459b4dcf05123476
Vishal Rathore <vishal352rathore@gmail.com>
2024-08-14 15:39:35 +0530

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -56,7 +56,7 @@ const Footer = () => {
         
         <div className="flex flex-col space-y-4 ">
           <p className="text-lg font-semibold cursor-pointer">Subscribe to receive deals and promotions</p>
-          <div className="bg-white rounded-3xl flex items-center w-[325px]">
+          <div className="bg-white rounded-3xl flex items-center w-[300px] md:w-[300px]">
             <input
               type="email"
               placeholder="Enter Email Address"
